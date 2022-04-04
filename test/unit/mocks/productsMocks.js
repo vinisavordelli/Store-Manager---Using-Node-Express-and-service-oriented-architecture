bb74ea6e463e675ea5cf56f82229d67b0a328908
Vinicius <vinicius.savordelli@gmail.com>
2022-04-04 19:01:38 -0300

    removes: all tests, lets start over

---
+++ test/unit/mocks/productsMocks.js
@@ -0,0 +1,26 @@
+const mockedAllProducts = [
+  {
+    id: 1,
+    name: "Martelo do Thor",
+    quantity: 10,
+  },
+  {
+    id: 3,
+    name: "Escudo do Capitão América",
+    quantity: 30,
+  },
+  {
+    id: 2,
+    name: "Traje de encolhimento",
+    quantity: 20,
+  },
+];
+
+const mockedProduct =   [{
+  id: 1,
+  name: "Martelo do Thor",
+  quantity: 10,
+}];
+
+module.exports ={mockedAllProducts, mockedProduct};
+
---
+++ test/unit/mocks/salesMocks.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    saleId: 1,
+    date: "2022-03-31 13:19:34",
+    productId: 1,
+    quantity: 5,
+  },
+  {
+    saleId: 1,
+    date: "2022-03-31 13:19:34",
+    productId: 20,
+    quantity: 10,
+  },
+  {
+    saleId: 2,
+    date: "2022-03-31 13:19:34",
+    productId: 3,
+    quantity: 15,
+  },
+];
